18c18b09ff07589f37c0585f86b39424b959aef5
Gabrielle <eastongabrielle@gmail.com>
2020-05-26 22:55:55 -0400

    fix hero button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 export default function Hero() {
   return (
-   
     <div className="hero">
       <div className="hero-info">
         <h1>Gabrielle Easton</h1>
         <h2>Software Developer</h2>
-        <button><a href="#projects">View my work</a></button>
+        <a className="hero-button" href="#projects">
+          View my work
+        </a>
       </div>
-      </div>
-   
+    </div>
   );
 }
