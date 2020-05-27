315e921a16653827a4aa06d0d10455bc814980d1
Gabrielle <eastongabrielle@gmail.com>
2020-05-27 00:52:53 -0400

    colors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,29 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
-      
-     <a className="social-media" href="https://github.com/GabrielleEaston" target="_blank" rel="noopener noreferrer"> <i className="fab fa-github"></i></a>
-     <a className="social-media" href="https://www.linkedin.com/in/gabrielle-easton" target="_blank" rel="noopener noreferrer"> <i className="fab fa-linkedin"></i></a> 
-        <a className="social-media" href="mailto:eastongabrielle@gmail.com"><i className="fas fa-envelope"></i></a>
+      <a
+        className="social-media"
+        href="https://github.com/GabrielleEaston"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {" "}
+        <i className="fab fa-github footer-icons"></i>
+      </a>
+      <a
+        className="social-media"
+        href="https://www.linkedin.com/in/gabrielle-easton"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {" "}
+        <i className="fab fa-linkedin footer-icons"></i>
+      </a>
+      <a className="social-media" href="mailto:eastongabrielle@gmail.com">
+        <i className="fas fa-envelope footer-icons"></i>
+      </a>
       <p>© 2020 by Gabrielle Easton All rights reserved.</p>
       <p>This website was built with ReactJs.</p>
-   </footer>
+    </footer>
   );
 }
