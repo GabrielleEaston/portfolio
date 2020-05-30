8a62e7244af124af059e3c5a4114d4c832804da5
Gabrielle <eastongabrielle@gmail.com>
2020-05-29 20:57:29 -0400

    add transition on a load

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Portfolio from './components/Portfolio';
-import About from './components/About';
-import Footer from './components/Footer';
-import MyForm from './components/Contact';
-
+import React from "react";
+import "./App.css";
+import { motion } from "framer-motion";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Portfolio from "./components/Portfolio";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import MyForm from "./components/Contact";
 
 function App() {
   return (
-    <div className="App">
+    <motion.div
+      className="App"
+      initial={{
+        opacity: 0,
+        x: 100}}
+      animate={{
+        opacity: 1,
+        x: 0}}
+      transition={{ duration: 1 }}
+    >
       <Header />
       <Hero />
       <About />
       <Portfolio />
       <MyForm />
       <Footer />
-
-    </div>
+    </motion.div>
   );
 }
 
