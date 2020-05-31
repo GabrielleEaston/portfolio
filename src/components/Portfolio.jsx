e1a740c336baf635fe4107427aa488c88b4d43e0
Gabrielle <eastongabrielle@gmail.com>
2020-05-30 22:14:50 -0400

    gifs shocase projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,27 +3,48 @@ import data from "../data.json";
 
 export default class Portfolio extends Component {
   state = {
-    projects: data
+    projects: data,
+    hovered: false
   };
 
   render() {
+  
     const { projects } = this.state;
     return (
-      <section className="projects" >
+      <section className="projects">
         <h2 id="projects">Projects</h2>
         <div className="grid">
-        {projects.map(project => (
-          
-          <div className="grid-item" key={project.id}>
-            <h3>{project.title}</h3>
-            <a href={project.url} target="_blank" rel="noopener noreferrer"><img src={project.image} alt={project.title} /></a>
-            <p className="desc">{project.description}</p>
-            <p className="technology">{project.technology}</p>
-            <a href={project.github} target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
-            <a href={project.url} target="_blank" rel="noopener noreferrer"><i className="fas fa-globe"></i></a>
-          </div>
-        ))}
-          </div>
+          {projects.map(project => (
+            <div className="grid-item" key={project.id}>
+              <h3>{project.title}</h3>
+              <a href={project.url} target="_blank" rel="noopener noreferrer">
+                <img
+                  src={project.image}
+                  alt={project.title}
+                  onMouseOver={e => (e.currentTarget.src = project.gif)}
+                  onMouseLeave={e => (e.currentTarget.src = project.image)}
+        //           onMouseOut={() => this.setState({hovered: false})}
+        // onMouseOver={() => this.setState({hovered: true})}
+       
+                 
+                />
+              
+              </a>
+              <p className="desc">{project.description}</p>
+              <p className="technology">{project.technology}</p>
+              <a
+                href={project.github}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fab fa-github"></i>
+              </a>
+              <a href={project.url} target="_blank" rel="noopener noreferrer">
+                <i className="fas fa-globe"></i>
+              </a>
+            </div>
+          ))}
+        </div>
       </section>
     );
   }
