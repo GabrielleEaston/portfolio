b8fa5bbd6b6dbf8eb42339187eb743bccd08c29b
Gabrielle <eastongabrielle@gmail.com>
2020-06-19 11:27:05 -0400

    container width changed to 70

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,6 @@ export default function Hero() {
     <div className="hero">
       <div className="hero-info">
         <h2>Let's build something together</h2>
-
         <p>Stay safe and keep creating</p>
       </div>
     </div>
