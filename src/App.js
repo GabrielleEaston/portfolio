11684c90215566f2460127a57bfd81ece32bc7fc
Gabrielle <eastongabrielle@gmail.com>
2020-08-14 16:11:29 -0400

    mailgo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
 import Sidebar from "./components/SideBar";
-import "mailgo/dist/mailgo.min.js";
+
 
 const App = () => {
   const [isOpen, setIsOpen] = useState(false);
