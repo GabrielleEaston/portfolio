39a62909ff28d2622903107fa584a438849d9d58
Gabrielle <eastongabrielle@gmail.com>
2020-09-16 21:41:57 -0400

    minimalistic form

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,15 +3,17 @@ import Portfolio from "./Portfolio";
 import Contact from "./Contact";
 import Hero from "./Hero";
 import About from "./About";
-import { Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 export default function Main() {
   return (
     <main>
-      <Route exact path="/" render={() => <Hero />} />
-      <Route exact path="/about" render={() => <About />} />
-      <Route exact path="/portfolio" render={() => <Portfolio />} />
-      <Route exact path="/contact" render={() => <Contact />} />
+      <Switch>
+        <Route exact path="/" render={() => <Hero />} />
+        <Route exact path="/about" render={() => <About />} />
+        <Route exact path="/portfolio" render={() => <Portfolio />} />
+        <Route exact path="/contact" render={() => <Contact />} />
+      </Switch>
     </main>
   );
 }
